8a5b599f2c880c8d93f41570d7634d703f0245de
P-O Quirion <pioliqui@gmail.com>
2021-06-10 09:15:03 -0400

    first commit

--- Puppetfile
+++ Puppetfile
@@ -3,3 +3,8 @@
 forge "https://forgeapi.puppetlabs.com"
 
 mod 'puppetlabs-stdlib', '5.2.0'
+mod 'puppet-epel', '3.0.1'
+mod 'puppet-logrotate', '5.0.0'
+mod 'puppet-nginx', '3.2.0'
+mod 'cmdntrf-consul_template', '2.3.0'
+mod 'KyleAnderson-consul', '6.1.0'
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,48 @@
-node default {
+class base {
+  include epel
+  package { 'python3': }
+  package { 'vim': }
+
+  $instances = lookup('terraform.instances')
+  $host_template = @(END)
+127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+<% @instances.each do |key, values| -%>
+<%= values['local_ip'] %> <%= key %> <% if values['tags'].include?('puppet') %>puppet<% end %>
+<% end -%>
+END
+
+  file { '/etc/hosts':
+    ensure  => file,
+    content => inline_template($host_template)
+  }
+
 
 }
 
+
+class proxy {
+  include nginx
+
+}
+
+
+
+node default {
+  include base
+
+  if 'puppet' in $instance_tags {
+    include profile::consul::server
+  } else {
+    include profile::consul::client
+  }
+
+  if 'bento' in $instance_tags {
+    include profile::singularity
+  }
+
+  if 'proxy' in $instance_tags {
+    include proxy
+  }
+
+
+}
